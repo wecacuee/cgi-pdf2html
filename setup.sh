bc2c01a3f5df72c653b7f8117e6e5b26f1fa4010
Vikas Dhiman <dhiman@umich.edu>
2019-05-07 23:51:32 -0700

    use up to run the service

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ command -v docker-compose || {
     echo "Install docker-compose" \
     && sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 }
-docker-compose run cgiserver
+docker-compose up
 
